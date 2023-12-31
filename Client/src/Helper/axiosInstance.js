9c7a4c3575c07c9a0ce00730e13be1250f6aae53
Tanishq Khandelwal <102526387+tanishq-khandelwal@users.noreply.github.com>
2023-12-31 23:23:22 +0530

    Update axiosInstance.js

--- Client/src/Helper/axiosInstance.js
+++ Client/src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ignite-learning.onrender.com";
+const BASE_URL = "https://ignite-learning.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
