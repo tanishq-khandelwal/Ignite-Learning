5bc2b203f0465d485c0230c50f011e023ae6b395
Tanishq Khandelwal <102526387+tanishq-khandelwal@users.noreply.github.com>
2024-01-01 07:20:20 +0000

    Update auth.middleware.js

--- Server/middlewares/auth.middleware.js
+++ Server/middlewares/auth.middleware.js
@@ -14,7 +14,6 @@ const isLoggedIn = async (req, res, next) => {
 
         // Setting the cookie within the context of the route handler
         const cookieOptions = {
-            resave: false,
             secure: true,
             sameSite: 'none',
             httpOnly: false
