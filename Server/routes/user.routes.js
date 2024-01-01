a27dbc87a63e164acf60f81516bc1a841f67d50d
Tanishq Khandelwal <102526387+tanishq-khandelwal@users.noreply.github.com>
2024-01-01 10:31:51 +0530

    Update user.routes.js

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -7,11 +7,11 @@ const router=Router();
 router.post('/register',upload.single("avatar"),registerUser);
 router.post('/login',loginUser);
 router.post('/logout',logoutUser);
-router.get('/me',getLoggedInUserDetails);
+router.get('/me',isLoggedIn,getLoggedInUserDetails);
 router.post("/reset", forgotPassword);
 router.post("/reset/:resetToken", resetPassword);
-router.post('/change-password',changePassword);
-router.put('/update/:id',upload.single("avatar"),updateUser);
+router.post('/change-password',isLoggedIn,changePassword);
+router.put('/update/:id',isLoggedIn,upload.single("avatar"),updateUser);
 
 
 export default router;
