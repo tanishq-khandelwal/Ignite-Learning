cccb8259baec9e7ce091fd6674fb29f90f2ae892
Tanishq Khandelwal <102526387+tanishq-khandelwal@users.noreply.github.com>
2024-01-01 09:03:40 +0530

    Update auth.middleware.js

--- Server/middlewares/auth.middleware.js
+++ Server/middlewares/auth.middleware.js
@@ -1,22 +1,30 @@
 import AppError from "../utils/error.utils.js";
 import jwt from 'jsonwebtoken';
 
-const isLoggedIn=async(req,res,next)=>{
-    const{token}=req.cookies;
+const isLoggedIn = async (req, res, next) => {
+    const { token } = req.cookies;
 
-    if(!token){
-        return next(new AppError('Unauthenticated,Please Login',401))
+    if (!token) {
+        return next(new AppError('Unauthenticated, Please Login', 401));
     }
 
     try {
         const userDetails = await jwt.verify(token, process.env.JWT_SECRET);
         req.user = userDetails;
+
+        // Setting the cookie within the context of the route handler
+        const cookieOptions = {
+            secure: true,
+            sameSite: 'None',
+        };
+        res.cookie('token', token, cookieOptions);
+
+        // Continue with the next middleware or route handler
         next();
     } catch (error) {
         return next(new AppError('Invalid or Expired Token', 401));
     }
-    
-}
+};
 
 const authorizedRoles=(...roles) => async(req,res,next) => {
     const currenUserRole = req.user.role;
